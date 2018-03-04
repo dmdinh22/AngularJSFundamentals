707199207696a3d4df6303fa24dbcb5073207fbc
David Dinh <dmdinh22@gmail.com>
2018-03-04 02:00:24 -0800

    event binding

--- 14-ng-submit/content/js/MoviesController.js
+++ 14-ng-submit/content/js/MoviesController.js
@@ -1,27 +1,27 @@
 function MoviesController() {
-  this.newTitle = '';
-  this.newRelease = '';
-  this.addMovie = function () {
-    this.favorites.unshift({
-      title: this.newTitle,
-      year: this.newRelease
-    })
-  };
-  this.favorites = [{
-    title: 'The Shawshank Redemption',
-    year: '1994'
-  }, {
-    title: 'Inception',
-    year: '2010'
-  },{
-    title: 'The Matrix',
-    year: '1999'
-  }, {
-    title: 'Saving Private Ryan',
-    year: '1998'
-  }];
+    this.newTitle = '';
+    this.newRelease = '';
+    this.addMovie = function () {
+        this.favorites.unshift({ // unshift to add to beginning of array
+            title: this.newTitle, // can use push to add at end
+            year: this.newRelease
+        });
+    };
+    this.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994'
+    }, {
+        title: 'Inception',
+        year: '2010'
+    },{
+        title: 'The Matrix',
+        year: '1999'
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    }];
 }
 
 angular
-  .module('app')
-  .controller('MoviesController', MoviesController);
+    .module('app')
+    .controller('MoviesController', MoviesController);
