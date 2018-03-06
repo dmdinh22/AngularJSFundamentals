c861931fd0d806c6a0742ae73cbee2b2c20e8017
David Dinh <dmdinh22@gmail.com>
2018-03-05 22:08:40 -0800

    added  api calls to app

--- Starter App/js/todo.controller.js
+++ Starter App/js/todo.controller.js
@@ -1,37 +1,69 @@
-function ToDoController() {
-    this.newToDo = '';
-    this.list = [{
-        title: 'First ToDo item',
-        completed: true
-    },
-    {
-        title: 'Second ToDo item',
-        completed: false
-    },
-    {
-        title: 'Third ToDo item',
-        completed: false
-    },];
+function ToDoController(ToDoService) {
+    var ctrl = this;
+    ctrl.newToDo = '';
+    ctrl.list = [];
+    
+    function getToDos() {
+        ToDoService
+            .retrieve()
+            .then(function(response){
+                ctrl.list = response;
+            });
+    } //getToDos
 
-    this.addToDo = function() {
-        this.list.unshift({
-            title: this.newToDo,
-            completed: false
-        });
-        this.newToDo = '';
+    ctrl.addToDo = function() {
+        // if no todo item (empty string), do not add
+        if (!ctrl.newToDo) {
+            return;
+        }
+        ToDoService
+            .create({
+                title: ctrl.newToDo,
+                completed: false
+            })
+            .then(function(response){
+                ctrl.list.unshift(response);
+            });
+        
+        ctrl.newToDo = '';
     };
 
-    this.removeToDo = function(item, index) {
-        this.list.splice(index, 1);
+    ctrl.removeToDo = function(item, index) {
+        // ctrl.list.splice(index, 1);
+        ToDoService
+            .remove(item)
+            .then(function(response){
+                ctrl.list.splice(index, 1);
+            });
     };
 
-    this.getRemaining = function() {
-        return this.list.filter(function(item){
+    ctrl.updateToDo = function(item, index) {
+        // if there is no title property, remove item
+        if (!item.title) {
+            ctrl.removeToDo(item, index);
+            return;
+        }
+        ToDoService
+            .update(item);
+    };
+    ctrl.getRemaining = function() {
+        return ctrl.list.filter(function(item){
             return !item.completed;
         });
     };
+    ctrl.toggleState = function(item) {
+        ToDoService
+            .update(item)
+            .then(function(){
+                // if everything is fine, do nothing
+            }, function(){
+                // if something went wrong, set back to false
+                item.completed = !item.completed;
+            });
+    };
+    getToDos();
 } //ToDoController
 
 angular
     .module('app')
-    .controller('ToDoController', ToDoController)
+    .controller('ToDoController', ToDoController);
